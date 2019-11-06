72c0ef02f8d810d1b4f298884ea9eafddbf477a8
BobWr <850775295@qq.com>
2019-11-06 14:45:18 +0800

    Create get_gpu.sh

---
+++ get_gpu.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SAR_DIR=$1
+DATA_DIR=$SAR_DIR/data
+
+touch $DATA_DIR/gpu.log
+
+while :
+do
+  nvidia-smi |awk '{print $13}' | awk '{{printf"%s",$0}}' |awk '{print}' >> $DATA_DIR/gpu.log
+  sleep 1
+done
