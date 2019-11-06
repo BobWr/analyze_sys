bd3c839f9b6614743e75d63e630190ad65bc2aba
BobWr <850775295@qq.com>
2019-11-06 16:52:27 +0800

    Update analyze_sys_start.sh

--- analyze_sys_start.sh
+++ analyze_sys_start.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 SAR_DIR=`pwd`
+sleep 1
 DATA_DIR=$SAR_DIR/data
+
 rm -rf $DATA_DIR/*
 
 sar -u 1 > $DATA_DIR/cpu.log &
 sar -n DEV 1 > $DATA_DIR/net.log &
 sar -r 1 > $DATA_DIR/mem.log &
 sar -b 1 > $DATA_DIR/io.log &
-/bin/sh $SAR_DIR/get_gpu.sh $SAR_DIR &
+/bin/sh $SAR_DIR/get_gpu.sh &
 
-DATA="Analyze start."
+DATA="DATA_DIR: $DATA_DIR.\r\n Analyze start.\r\n"
 length=${#DATA}
 echo -en "HTTP/1.1 200 OK\r\n"
 echo -en "Content-Type: text/plain\r\n" 
